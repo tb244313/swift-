2e6b6c99e672596911e87605339ecb37f64b0e4e
apple <1455421519@qq.com>
2017-08-07 16:24:21 +0800

    1.int double  变量 常量的基本用法

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Hello World
+var str = "Hello, playground"
+print("Hello World!xxxx")
+
+//常量
+let 苹果多少钱一斤 = 5
+let 工资 = 5000
+
+
+//变量
+var 课程名 = "操作系统"
+课程名 = "swift"
+
+//类型推断 Int 可以省略不写
+var 今天跑步多少公里 : Int = 5
+
+//double
+var 一杯水 = 5.5
+
+//一但定义类型   则不能更改  类型安全
+var 电费 = 3.5
+电费 = 5.0
+var 课程名 = (day:3,unit:"天",lang:"Swift",ver:"3.0")
+
+print(电费)
+
